de4f79b642b2f6298a5c6457a161c859e4eaa209
vv1909 <vv1909vv@gmail.com>
2026-03-06 19:27:27 +0400

    Add files via upload

---
+++ tourw.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int dir[8][2] = {
+    {2,1},{1,2},{-1,2},{-2,1},
+    {-2,-1},{-1,-2},{1,-2},{2,-1}
+};
+
+int countMoves(int board[8][8], int n, int x, int y) {
+
+    int count = 0;
+
+    for (int i = 0; i < 8; i++) {
+
+        int nx = x + dir[i][0];
+        int ny = y + dir[i][1];
+
+        if (nx >= 0 && ny >= 0 && nx < n && ny < n && board[nx][ny] == -1)
+            count++;
+    }
+
+    return count;
+}
+
+int getSortedMoves(int board[8][8], int n, int x, int y, int moves[8]) {
+
+    int degree[8];
+    int moveCount = 0;
+
+    for (int i = 0; i < 8; i++) {
+
+        int nx = x + dir[i][0];
+        int ny = y + dir[i][1];
+
+        if (nx >= 0 && ny >= 0 && nx < n && ny < n && board[nx][ny] == -1) {
+
+            moves[moveCount] = i;
+            degree[moveCount] = countMoves(board, n, nx, ny);
+            moveCount++;
+        }
+    }
+
+    // randomize first
+    for (int i = 0; i < moveCount; i++) {
+        int r = rand() % moveCount;
+        swap(moves[i], moves[r]);
+        swap(degree[i], degree[r]);
+    }
+
+    // sort by degree (Warnsdorff rule)
+    for (int i = 0; i < moveCount - 1; i++) {
+        for (int j = i + 1; j < moveCount; j++) {
+
+            if (degree[j] < degree[i]) {
+
+                swap(degree[i], degree[j]);
+                swap(moves[i], moves[j]);
+            }
+        }
+    }
+
+    return moveCount;
+}
+
+bool knightTour(int board[8][8], int n, int x, int y, int step) {
+
+    if (step == n * n)
+        return true;
+
+    int moves[8];
+
+    int moveCount = getSortedMoves(board, n, x, y, moves);
+
+    for (int i = 0; i < moveCount; i++) {
+
+        int dirIdx = moves[i];
+
+        int nx = x + dir[dirIdx][0];
+        int ny = y + dir[dirIdx][1];
+
+        board[nx][ny] = step;
+
+        if (knightTour(board, n, nx, ny, step + 1))
+            return true;
+
+        board[nx][ny] = -1;
+    }
+
+    return false;
+}
+
+int main() {
+
+    srand(time(0));
+
+    int n = 8;
+    int startx, starty;
+
+    cout << "Input coordinates (0-7) (0-7): ";
+    cin >> startx >> starty;
+
+    int board[8][8];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            board[i][j] = -1;
+
+    board[startx][starty] = 0;
+
+    if (knightTour(board, n, startx, starty, 1)) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++)
+                cout << setw(4) << board[i][j];
+
+            cout << endl;
+        }
+
+    } else {
+        cout << "No solution";
+    }
+
+    return 0;
+}
